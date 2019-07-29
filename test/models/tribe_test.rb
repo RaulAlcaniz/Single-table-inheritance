2e9f7c67941318427e9cb70e9370a1bb26ed2574
raul <raul@secret-source.eu>
2019-07-29 15:35:02 +0100

    Added relationship between tribe and animals with its test. Also gem for using rails console has been added

--- test/models/tribe_test.rb
+++ test/models/tribe_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TribeTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "Tribe has to have many animals" do
+    assert_equal Tribe.reflect_on_association(:animals).macro, :has_many
   end
 end
