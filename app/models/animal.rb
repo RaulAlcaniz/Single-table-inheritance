01463d30988f4aada8b1c1c8ebcb559347b8800f
raul <raul@secret-source.eu>
2019-07-29 16:09:24 +0100

    Added test and functionality for races in Animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :tribe
+
+  self.inheritance_column = :race
+
+  def self.races
+    %w(Lion WildBoard Meerkat)
+  end
 end
--- test/models/animal_test.rb
+++ test/models/animal_test.rb
@@ -4,4 +4,8 @@ class AnimalTest < ActiveSupport::TestCase
   test 'Animals should be belongs tribe' do
     assert_equal Animal.reflect_on_association(:tribe).macro, :belongs_to
   end
+
+  test 'Animals should have 3 races of animals' do
+    assert_equal Animal.races, %w( Lion WildBoard Meerkat )
+  end
 end
